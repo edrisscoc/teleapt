1ecade6a23efa56649318a7a0c9065b1023b21b7
edrisscoc <edriss.coc@gmail.com>
2016-01-27 20:19:03 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -235,8 +235,8 @@ function create_config( )
   print ('saved config into ./data/config.lua')
 end
 
-function on_our_id (id)
-  123043670 = id
+function 123043670 (id)
+  on_our_id = id
 end
 
 function on_user_update (user, what)
